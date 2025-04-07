0568533f295f687a25b9b7132432c8a6df66bf80
Tim Burkei <tim.burkei@accoria.de>
2025-04-07 03:55:17 +0200

    remove wrong node count

--- IaC-1/aks.tf
+++ IaC-1/aks.tf
@@ -6,7 +6,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster_thesis_hdm_25" {
 
   default_node_pool {
     name = "default"
-    node_count = 1
+    node_count = 2
     vm_size        = "Standard_DS2_v2"
     vnet_subnet_id = azurerm_subnet.aks_subnet.id
 
